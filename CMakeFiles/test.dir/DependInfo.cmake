7fccd13f2f59c0ec0b091f8181199989d7dda44e
DaewonLee <lee.daewon@gmail.com>
2015-09-10 16:21:23 -0400

    included some headers

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daewon/workspace/imu_odroid/imu.cpp" "/home/daewon/workspace/imu_odroid/CMakeFiles/test.dir/imu.cpp.o"
+  "/home/daewon/workspace/imu_odroid/motor.cpp" "/home/daewon/workspace/imu_odroid/CMakeFiles/test.dir/motor.cpp.o"
+  "/home/daewon/workspace/imu_odroid/motor_test.cpp" "/home/daewon/workspace/imu_odroid/CMakeFiles/test.dir/motor_test.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
